8db8ec42e8a29c61d86ee4c8631d9486f5e6a82f
Fernndo Viton <fcode@viton.org>
2018-12-23 20:42:55 -0800

    doesn't work

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,12 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, compose, createStore } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
 import App from './App';
 import middlewares from './middleware/index';
-import reducer from './reducers/index';
+import rootReducer from './reducers/index';
 import registerServiceWorker from './registerServiceWorker';
 
 const composeEnhancers:  <R>(a: R) => R =
@@ -21,11 +24,20 @@ const enhancers = composeEnhancers(
     // other store enhancers if any
   );
 
-const store = createStore(reducer, enhancers);
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+const store = createStore(persistedReducer, enhancers);
+const persistor = persistStore(store);
 
 ReactDOM.render(
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <App />
+      </PersistGate>
     </Provider>,
     document.getElementById('root'));
 
